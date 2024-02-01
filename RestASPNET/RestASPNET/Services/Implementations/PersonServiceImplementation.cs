9f4dbf8134294521eeba7f70f4b30805b9753377
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-02-01 16:56:15 -0300

    feat: add findById and Person Mock

--- RestASPNET/RestASPNET/Services/Implementations/PersonServiceImplementation.cs
+++ RestASPNET/RestASPNET/Services/Implementations/PersonServiceImplementation.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using RestASPNET.Model;
+using RestASPNET.Services;
 
 namespace RestASPNET.Services.Implementations
 {
     public class PersonServiceImplementation : IPersonService
     {
+        private volatile int count;
+
         public Person Create(Person person)
         {
             return person;
@@ -20,17 +23,48 @@ namespace RestASPNET.Services.Implementations
 
         public List<Person> FindAll()
         {
-            throw new NotImplementedException();
+            List<Person> persons = [];
+            
+            for(int i = 0; i < 8; i++) {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+
+            return persons;
         }
 
         public Person FindByID(long id)
         {
-            throw new NotImplementedException();
+            return new Person
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Address = "São Paulo",
+                Gender = "Male"
+            };
         }
 
         public Person Update(Person person)
         {
             throw new NotImplementedException();
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Person LastName" + i,
+                Address = "Some Address" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
         }
     }
 }
